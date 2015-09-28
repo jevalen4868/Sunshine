7ed6ed4274c68e2d42578d06e1d9c6622c8e27fe
jevalen4868 <jevalen4868@yahoo.com>
2015-09-27 19:15:29 -0500

    min length

--- app/src/main/java/com/fallenman/jeremyvalenzuela/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/fallenman/jeremyvalenzuela/sunshine/app/LocationEditTextPreference.java
@@ -1,9 +1,16 @@
 package com.fallenman.jeremyvalenzuela.sunshine.app;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by jeremyvalenzuela on 9/22/15.
@@ -24,4 +31,39 @@ public class LocationEditTextPreference extends EditTextPreference{
             a.recycle();
         }
     }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        EditText et = getEditText();
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog d = getDialog();
+                if(d instanceof AlertDialog) {
+                    AlertDialog dialog = (AlertDialog)d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if text input is too short.
+                    if( s.length() < mMinLength ) {
+                        positiveButton.setEnabled(false);
+                    }
+                    else {
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
+
+
 }
